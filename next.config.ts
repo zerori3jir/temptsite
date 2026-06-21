488dd16134ec553c400ea40e37d35fcca55cd515
zero <62719887+zerori3jir@users.noreply.github.com>
2026-06-21 13:51:02 -0400

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist'
+}
+module.exports = nextConfig
+
 
 export default nextConfig;
